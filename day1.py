f65e8132c0311d4b465776a9c407917d8f690db0
Júlia Mateini <julia@mateini.com>
2026-03-07 18:26:04 -0300

    Fix comment for band name generator

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-# Band generator
+# Band name generator
 print("Welcome to the band name generator.")
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of the pet?\n")
